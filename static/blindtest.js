329022074cb0d6e2f2e931a546419dfb80b1d2ef
pemaxime <maxxes@MacBook-Air-de-Maxime.local>
2024-05-02 15:17:52 +0200

    feat function verify and restart

--- static/blindtest.js
+++ static/blindtest.js
@@ -22,3 +22,27 @@ function togglePlayPause() {
         audioPlayer.pause();
     }
 }
+
+function verifyAnswer(guessTrack) {
+    var resultDiv = document.getElementById("result");
+
+    var correctTrackName = document.getElementById('blindtest').getAttribute('data-track');
+
+    if (guessTrack.toLowerCase() === correctTrackName.toLowerCase()) {
+        resultDiv.innerHTML = "Félicitations ! Vous avez deviné la chanson correctement : " + correctTrackName;
+    } else {
+        resultDiv.innerHTML = "Désolé, votre réponse est incorrecte.";
+    }
+}
+
+document.getElementById("blindTestForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var formData = new FormData(event.target);
+    var guessTrack = formData.get("guessTrack");
+
+    verifyAnswer(guessTrack);
+});
+
+function restartGame() {
+    location.reload();
+}
